67aa832b4016deb692f8c8d546286eed8a660bd4
wangbourne <wangbourne@gmail.com>
2013-03-02 19:50:26 +1000

    Summary: disable rotation during play mode

---
+++ PlayViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlayViewController.h
+//  FlashCardCreator
+//
+//  Created by Wang Bourne on 2/03/13.
+//  Copyright (c) 2013 Internetics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayViewController : UIViewController
+
+@end
---
+++ PlayViewController.m
@@ -0,0 +1,38 @@
+//
+//  PlayViewController.m
+//  FlashCardCreator
+//
+//  Created by Wang Bourne on 2/03/13.
+//  Copyright (c) 2013 Internetics. All rights reserved.
+//
+
+#import "PlayViewController.h"
+
+@interface PlayViewController ()
+
+@end
+
+@implementation PlayViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
