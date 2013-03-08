236bc1542d0e4343f630ee9fd8e36a9388019ee0
wangbourne <wangbourne@gmail.com>
2013-03-08 16:05:30 +1000

    Summary: fix a literal issue

--- FlashCardCreator/AppDelegate.m
+++ FlashCardCreator/AppDelegate.m
@@ -147,7 +147,7 @@ extern BOOL isLoggingDropboxInSettingView;
     //11. Get example packs (online) and save to local
     BOOL isExamplePackDownloadedSuccessful = [[NSUserDefaults standardUserDefaults] boolForKey:@"isExamplePackDownloadedSuccessful"];
     if (isExamplePackDownloadedSuccessful ==NO) {
-        [[NSNotificationCenter defaultCenter] postNotificationName:DOWNLOAD_PACK_NOTIFICATION object:@"https://www.dropbox.com/s/h34cq4xzpnkl8br/card1362721831.263798-1025516636.zi"];
+        [[NSNotificationCenter defaultCenter] postNotificationName:DOWNLOAD_PACK_NOTIFICATION object:@"https://www.dropbox.com/s/h34cq4xzpnkl8br/card1362721831.263798-1025516636.zip"];
     }
     
     return YES;
