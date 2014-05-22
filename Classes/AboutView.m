d05c92482552e6fa9111cd98b01b50f0960054bb
Liang Wang <wangbourne@gmail.com>
2014-05-22 11:38:07 +1000

    update

--- Classes/AboutView.m
+++ Classes/AboutView.m
@@ -24,7 +24,7 @@
         }
         
 		if (isUserInterfaceIdiomPhone) {
-            textView = [[UITextView alloc] initWithFrame:CGRectMake(0, 0, width, 180)];    
+            textView = [[UITextView alloc] initWithFrame:CGRectMake(0, 0, width, 205)];
         } else {
             textView = [[UITextView alloc] initWithFrame:CGRectMake(0, 0, width, 260)];
         }
@@ -32,7 +32,7 @@
 		textView.backgroundColor = [UIColor clearColor];
 		textView.font = [UIFont fontWithName:@"Helvetica-Bold" size:14];
 		//textView.text = @"Direct Digital is a smart publisher that publishes smart things like this app. Based in Brisbane, Australia, Direct Digital likes doing things that other people like us doing. Another thing that people seem to like is the\n\n\nwebsite. Check it out and see if you like to too.";
-		textView.text = @"Flash Card Creator has been developed by Flip Flash Cards Pty Ltd in conjunction with development team Internetics Pty Ltd.\nCopyright Flip Flash Cards 2013. All rights reserved.\nFor information on how to use the application, please click the (i) information button above.\nPlease submit any technical feedback to fcc@internetics.net.au\nThank you for your interest in Flash Card Creator and Flip Flash Cards Pty Ltd.";
+		textView.text = @"Flash Card Creator has been developed by Flip Flash Cards Pty Ltd in conjunction with development team Internetics Pty Ltd.\n\nCopyright Flip Flash Cards 2013. All rights reserved.\n\nFor information on how to use the application, please click the (i) information button above.\n\nPlease submit any technical feedback to fcc@internetics.net.au\n\nThank you for your interest in Flash Card Creator and Flip Flash Cards Pty Ltd.";
 		textView.textColor = [UIColor whiteColor];
         textView.userInteractionEnabled = false;
 		[self addSubview:textView];
@@ -43,11 +43,11 @@
 		linkLabel.textColor = [UIColor colorWithRed:0.9 green:0.25 blue:0.59 alpha:1.0];
 		linkLabel.font = [UIFont fontWithName:@"Helvetica-Bold" size:12];
 		linkButton = [UIButton buttonWithType:UIButtonTypeCustom];
-		linkButton.frame = CGRectMake(0, (textView.frame.origin.y + textView.frame.size.height + 10), 300, 20);
+		linkButton.frame = CGRectMake(0, (textView.frame.origin.y + textView.frame.size.height + 5), 300, 20);
 		[linkButton addSubview:linkLabel];
 		[self addSubview:linkButton];
         
-        UILabel *verionLable = [[UILabel alloc] initWithFrame:CGRectMake(0, (linkButton.frame.origin.y + linkButton.frame.size.height + 20), width, 20)];
+        UILabel *verionLable = [[UILabel alloc] initWithFrame:CGRectMake(0, (linkButton.frame.origin.y + linkButton.frame.size.height), width, 20)];
 		verionLable.backgroundColor = [UIColor clearColor];
 		verionLable.text = [NSString stringWithFormat:@"Version: %@",[Common appVersion]];
         verionLable.textAlignment = UITextAlignmentCenter;
@@ -55,7 +55,7 @@
 		verionLable.font = [UIFont fontWithName:@"Helvetica-Bold" size:12];
         [self addSubview:verionLable];
         
-        UILabel *buildLable = [[UILabel alloc] initWithFrame:CGRectMake(0, (verionLable.frame.origin.y + verionLable.frame.size.height + 10), width, 20)];
+        UILabel *buildLable = [[UILabel alloc] initWithFrame:CGRectMake(0, (verionLable.frame.origin.y + verionLable.frame.size.height), width, 20)];
 		buildLable.backgroundColor = [UIColor clearColor];
 		buildLable.text = [NSString stringWithFormat:@"Build: %@",[Common build]];
         buildLable.textAlignment = UITextAlignmentCenter;
@@ -63,7 +63,7 @@
 		buildLable.font = [UIFont fontWithName:@"Helvetica-Bold" size:12];
         [self addSubview:buildLable];
         
-        UILabel *sqliteLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, (buildLable.frame.origin.y + verionLable.frame.size.height + 10), width, 20)];
+        UILabel *sqliteLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, (buildLable.frame.origin.y + verionLable.frame.size.height), width, 20)];
 		sqliteLabel.backgroundColor = [UIColor clearColor];
 		sqliteLabel.text = [NSString stringWithFormat:@"SQlite: %d",[Common currentInstalledSqliteVersion]];
         sqliteLabel.textAlignment = UITextAlignmentCenter;
