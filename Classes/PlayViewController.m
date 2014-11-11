699331473a3e74d6fa47b19fe01f318a5b2c696b
Liang Wang <wangbourne@gmail.com>
2014-11-11 16:37:23 +1000

    small adjustment

--- Classes/PlayViewController.m
+++ Classes/PlayViewController.m
@@ -424,7 +424,7 @@
     
     
     if ([[NSUserDefaults standardUserDefaults] boolForKey:@"isTextToSpeech"]) {
-        double delayInSeconds = 1.0;
+        double delayInSeconds = 0.5;
         dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
         dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
             [_currentFlashCardView textToSpeechAllContentNow];
@@ -469,7 +469,7 @@
 
     
     if ([[NSUserDefaults standardUserDefaults] boolForKey:@"isTextToSpeech"]) {
-        double delayInSeconds = 1.0;
+        double delayInSeconds = 0.5;
         dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
         dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
             [_currentFlashCardView textToSpeechAllContentNow];
