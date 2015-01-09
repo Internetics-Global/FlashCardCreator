8442280601f02fd649c1a4f3515e541b2561198d
Liang Wang <wangbourne@gmail.com>
2015-01-09 23:08:27 +1000

    update visual

--- FlashCardCreator/MasterViewController.m
+++ FlashCardCreator/MasterViewController.m
@@ -444,7 +444,7 @@ enum popover_enum {
     self.view.userInteractionEnabled = NO;
     
     
-    double delayInSeconds = 0.8;
+    double delayInSeconds = 0.0;
     dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
     dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
         [_backgroundOfCreateCardView removeFromSuperview];
