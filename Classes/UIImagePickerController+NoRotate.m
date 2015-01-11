91e716b251aa25b2c5a6cca264d478fd946cfe2e
Liang Wang <wangbourne@gmail.com>
2015-01-11 10:09:04 +1000

    allow to use landscape for UIImagePickerController

--- Classes/UIImagePickerController+NoRotate.m
+++ Classes/UIImagePickerController+NoRotate.m
@@ -14,8 +14,16 @@
 //See the background for doing this
 //http://stackoverflow.com/questions/12540597/supported-orientations-has-no-common-orientation-with-the-application-and-shoul
 
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation {
+    return UIInterfaceOrientationIsLandscape(toInterfaceOrientation);
+}
+
 - (BOOL)shouldAutorotate {
-    return NO;
+    return YES;
+}
+
+- (NSUInteger)supportedInterfaceOrientations {
+    return UIInterfaceOrientationMaskLandscape;
 }
 
 @end
