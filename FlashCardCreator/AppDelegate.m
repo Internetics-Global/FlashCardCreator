6af44dd2dc0ee1d1a529cd8913ab689c4bc6b81f
Liang Wang <wangbourne@gmail.com>
2015-05-26 18:50:43 +1000

    update

--- FlashCardCreator/AppDelegate.m
+++ FlashCardCreator/AppDelegate.m
@@ -202,7 +202,7 @@ BOOL _isDownloadingSamplePack;
     BOOL isExamplePackDownloadedSuccessful = [[NSUserDefaults standardUserDefaults] boolForKey:@"isExamplePackDownloadedSuccessful"];
     if (isExamplePackDownloadedSuccessful ==NO) {
         _isDownloadingSamplePack = TRUE;
-        [[NSNotificationCenter defaultCenter] postNotificationName:DOWNLOAD_PACK_NOTIFICATION object:@"https://www.dropbox.com/s/szccw743kb41xol/Pack1421026646333651989.zip?type=demo"];
+        [[NSNotificationCenter defaultCenter] postNotificationName:DOWNLOAD_PACK_NOTIFICATION object:@"https://s3.amazonaws.com/internetics.flashcardcreator/Sample_25052015.zip?type=demo"];
     } else {
         _isDownloadingSamplePack = FALSE;
     }
