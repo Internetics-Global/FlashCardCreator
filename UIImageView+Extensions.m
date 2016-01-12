a8f81e73f0764f1e9d49cce8e218f6e4daaed411
Internetics <BourneWang@Bournes-MacBook-Pro.local>
2016-01-12 15:24:25 +0800

    fix a bug in youtube icon clickable area

--- UIImageView+Extensions.m
+++ UIImageView+Extensions.m
@@ -23,6 +23,13 @@ static const NSString *KEY_HIT_TEST_EDGE_INSETS = @"HitTestEdgeInsets";
 }
 
 - (BOOL)pointInside:(CGPoint)point withEvent:(UIEvent *)event {
+    
+    BOOL transparent = [self transparentAtPoint:point];
+    
+    if (transparent == YES) {
+        return false;
+    }
+    
     if(UIEdgeInsetsEqualToEdgeInsets(self.hitTestEdgeInsets, UIEdgeInsetsZero) || self.hidden) {
         return [super pointInside:point withEvent:event];
     }
@@ -31,6 +38,33 @@ static const NSString *KEY_HIT_TEST_EDGE_INSETS = @"HitTestEdgeInsets";
     CGRect hitFrame = UIEdgeInsetsInsetRect(relativeFrame, self.hitTestEdgeInsets);
     
     return CGRectContainsPoint(hitFrame, point);
+}
+
+
+
+/**
+ *  Special in this project, we ignore those image location with alpha value = 0
+ */
+- (BOOL) transparentAtPoint:(CGPoint)point
+{
+    unsigned char pixel[4] = {0};
+    
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    
+    CGContextRef context = CGBitmapContextCreate(pixel, 1, 1, 8, 4, colorSpace, kCGBitmapAlphaInfoMask & kCGImageAlphaPremultipliedLast);
+    
+    CGContextTranslateCTM(context, -point.x, -point.y);
+    
+    [self.layer renderInContext:context];
+    
+    CGContextRelease(context);
+    CGColorSpaceRelease(colorSpace);
+    
+    //NSLog(@"pixel: %d %d %d %d", pixel[0], pixel[1], pixel[2], pixel[3]);
+    
+    UIColor *color = [UIColor colorWithRed:pixel[0]/255.0 green:pixel[1]/255.0 blue:pixel[2]/255.0 alpha:pixel[3]/255.0];
+    
+    return pixel[3] <0.01;
 }
 
 @end
