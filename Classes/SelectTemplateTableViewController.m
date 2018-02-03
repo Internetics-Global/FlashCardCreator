be7faadb9f39d7e4d230815d22fd06131a6b4473
liang.wang <liang.wang@ladbrokes.com.au>
2018-02-03 21:38:38 +1000

    fix a ui bug in template select view

--- Classes/SelectTemplateTableViewController.m
+++ Classes/SelectTemplateTableViewController.m
@@ -31,6 +31,7 @@
 {
     [super viewDidLoad];
     self.title = NSLocalizedString(@"Title_Select_Template",@"");
+    self.tableView.separatorColor = [UIColor clearColor];
     self.view.backgroundColor = [UIColor colorWithRed:51.0/255 green:51.0/255 blue:51.0/255 alpha:1];
     
 }
